ab14959a048194d28091f938dd1c81e14ac59dc8
Farouk Yusuf <faroukyusuf.code@gmail.com>
2024-07-01 23:08:00 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+function date() {
+  var today = new Date();
+  document.getElementById("date").innerHTML = today.toDateString();
+}
+
+function clock() {
+  var today = new Date();
+  var hour = zeros(twelveHour(today.getHours()));
+  var minutes = zeros(today.getMinutes());
+  var seconds = zeros(today.getSeconds());
+  if (today.getHours() >= 12) {
+    seconds += " PM";
+  } else {
+    seconds += " AM";
+  }
+  hrs = today.getHours();
+  if (hrs < 12) greet = "Good Morning, Farouk";
+  else if (hrs >= 12 && hrs <= 17) greet = "Good Afternoon, Farouk";
+  else if (hrs >= 17 && hrs <= 24) greet = "Good Evening, Farouk";
+  document.getElementById("greet").innerHTML = greet;
+  document.getElementById("hour").innerHTML = hour;
+  document.getElementById("min").innerHTML = minutes;
+  document.getElementById("sec").innerHTML = seconds;
+}
+
+function twelveHour(hour) {
+  if (hour > 12) {
+    return (hour -= 12);
+  } else if (hour === 0) {
+    return (hour = 12);
+  } else {
+    return hour;
+  }
+}
+
+function zeros(num) {
+  if (num < 10) {
+    num = "0" + num;
+  }
+  return num;
+}
+
+function dateTime() {
+  date();
+  clock();
+  setTimeout(dateTime, 500);
+}
+
+dateTime();
+
+// Light and Dark Mode Support
+const darkMode =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+if (darkMode) {
+  document.documentElement.style.setProperty("--bg-color", "#121212");
+  document.documentElement.style.setProperty("--text-color", "#ffffff");
+  document.documentElement.style.setProperty("--card-bg-color", "#333333");
+  document.documentElement.style.setProperty("--card-text-color", "#ffffff");
+} else {
+  document.documentElement.style.setProperty("--bg-color", "#ffffff");
+  document.documentElement.style.setProperty("--text-color", "#000000");
+  document.documentElement.style.setProperty("--card-bg-color", "#553c4e");
+  document.documentElement.style.setProperty("--card-text-color", "#ffffff");
+}
